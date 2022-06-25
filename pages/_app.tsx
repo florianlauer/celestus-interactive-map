861a91ae36fa0a5fa605419156d6f182d578617c
Florian Lauer <florian@sencrop.com>
2022-06-25 19:00:21 +0000

    feat(map): add leaflet map

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "../theme";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/components/Map/Map.tsx
@@ -0,0 +1,22 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+
+const Map = () => {
+  return (
+    <MapContainer
+      center={[40.8054, -74.0241]}
+      zoom={14}
+      scrollWheelZoom={true}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  );
+};
+
+export default Map;
